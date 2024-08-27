071688b8103fd3f863b914185bff67e3638bb70e
akai kaede <genko2@hotmail.com>
2024-08-27 11:12:35 -0700

    add one line editor

--- packages/vite-tiptap/src/AppMantine.tsx
+++ packages/vite-tiptap/src/AppMantine.tsx
@@ -1,14 +1,18 @@
 import "@mantine/core/styles.css";
-import '@mantine/tiptap/styles.css';
+import "@mantine/tiptap/styles.css";
 
-import {MantineProvider} from "@mantine/core";
-import {theme} from "./theme";
-import {DefaultTextEditor} from "./DefaultTextEditor.tsx";
-import {DemoStyles} from "./DemoStyles.tsx";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
+// import {DefaultTextEditor} from "./DefaultTextEditor.tsx";
+import { OneLineEditor } from "./OneLineEditor.tsx";
+import { DemoStyles } from "./DemoStyles.tsx";
 
 export default function AppMantine() {
-  return (<MantineProvider theme={theme}>
-    <DefaultTextEditor/>
-    <DemoStyles/>
-  </MantineProvider>);
+  return (
+    <MantineProvider theme={theme}>
+      {/*<DefaultTextEditor/>*/}
+      <OneLineEditor />
+      <DemoStyles collapsed={false} />
+    </MantineProvider>
+  );
 }
---
+++ packages/vite-tiptap/src/OneLineEditor.tsx
@@ -0,0 +1,34 @@
+import { RichTextEditor } from "@mantine/tiptap";
+import { useEditor } from "@tiptap/react";
+import { Doc } from "./Doc";
+
+import { Text } from "@tiptap/extension-text";
+import { Paragraph } from "@tiptap/extension-paragraph";
+import { Mention } from "@tiptap/extension-mention";
+import classes from "./DefaultTextEditor.module.css";
+import suggestion from "./suggestion.ts";
+import { Button } from "@mantine/core";
+
+
+export function OneLineEditor() {
+  const editor = useEditor({
+    extensions: [
+      Doc,
+      Text,
+      Paragraph,
+      Mention.configure({
+        HTMLAttributes: {
+          class: classes.mention
+        },
+        suggestion
+      })
+    ],
+    content: "this is test."
+  });
+  return (<>
+    <RichTextEditor editor={editor}>
+      <RichTextEditor.Content/>
+    </RichTextEditor>
+    <Button onClick={() => console.log(editor?.getJSON())}/>
+  </>);
+}
